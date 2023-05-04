7b8eee257323573329d03b088fa6a1eae2e6c31e
saulvegasauceda <86434516+saulvegasauceda@users.noreply.github.com>
2023-05-04 16:44:58 -0400

    draft of magic imputation run on merfish

---
+++ 6S052_project/magic.py
@@ -0,0 +1,41 @@
+import magic
+import scanpy as sc
+import numpy as np
+import scanpy.external as sce
+
+def magic_imputation(t, counts_adata, target_sum, output_path):
+    """
+    Run MAGIC modifying only t and knn_dist
+    saves imputed and imputed-rescaled counts as h5ad
+
+    output_path: should end with "/"
+    """
+    assert output_path[-1] == "/", "output_path must end with forwardslash '/'"
+
+    magic_file = output_path + f"magic_t={t}_imputed.h5ad"
+    
+    print(30*'-', flush=True)
+    print("t:", t, flush=True)
+    print("knn_dist:", knn_dist, flush=True)
+    print(30*'-', flush=True)
+
+    dropout_adata = counts_adata.X.copy()
+    sc.pp.sqrt(dropout_adata) # sqrt transformation
+
+    magic_adata = sce.pp.magic(
+        adata = dropout_adata, 
+        name_list = 'all_genes',
+        solver = 'exact',
+        t = t, 
+        knn_dist = knn_dist, 
+        n_jobs = -1, 
+        copy = True,
+        verbose = True
+    )
+
+    magic_adata.X = np.square(magic_adata.X)
+
+    sc.pp.normalize_total(magic_adata, target_sum=target_sum, exclude_highly_expressed=False)
+
+    print("Saving h5ad of imputed counts...", flush=True)
+    magic_adata.write_h5ad(magic_file, compression='gzip')
---
+++ 6S052_project/run_magic.py
@@ -0,0 +1,42 @@
+from numpy.random import seed
+from multiprocessing import Pool
+from functools import partial
+import scanpy as sc
+from magic import magic_imputation
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+
+if __name__ == '__main__':
+    seed(1738)
+    TARGET_SUM = 500
+    NUMBER_OF_CELLS = 10_000
+    CAPTURE_RATE = 0.30
+
+    # getting files
+    path_to_dir = "/Users/saulvegasauceda/Documents/Spring_23/6.S052/data/"
+    output_path = "/Users/saulvegasauceda/Documents/Spring_23/6.S052/data/magic/"
+    dropout_file = path_to_dir + "dropout_capture_rate=0.3.h5ad"
+    merfish_file = path_to_dir + "merfish_norm.h5ad"
+
+    merfish = sc.read_h5ad(merfish_file)
+    dropout_adata = sc.read_h5ad(dropout_file)
+
+    # Setup for grid search for MAGIC
+    param_grid = [i for i in range(0, 10)]
+
+    # using partial function to pass in default params
+    run_imputation_on_dropout = partial(
+        magic_imputation,
+        counts_adata=dropout_adata,
+        target_sum=TARGET_SUM,
+        output_path=output_path
+    )
+
+    print("Processing running NMF imputation...")
+    CPUS_TO_USE = os.cpu_count() // 3
+    with Pool(CPUS_TO_USE) as p:
+        p.map(run_imputation_on_dropout, param_grid)
+
+    print("Done!")
