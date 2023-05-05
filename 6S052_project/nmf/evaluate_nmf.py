03231c3e7098a56926dc2cfd185d87f002b08307
saulvegasauceda <86434516+saulvegasauceda@users.noreply.github.com>
2023-05-04 20:39:01 -0400

    successfull run of evaluation for nmf

--- 6S052_project/nmf/evaluate_nmf.py
+++ 6S052_project/nmf/evaluate_nmf.py
@@ -2,15 +2,17 @@ from numpy.random import seed
 from multiprocessing import Pool
 from functools import partial
 import scanpy as sc
-from nmf import nmf_imputation
 import numpy as np
+import pandas as pd
 import os
 import warnings
 
+
 def rmse(dropout_file, true_counts_adata):
-    imputed_counts = sc.read_h5ad(dropout_file) 
-    imputed, merfish = imputed.X, true_counts_adata.X
-    return np.sqrt(np.square(np.subtract(imputed,merfish)).mean())
+    imputed_counts = sc.read_h5ad(dropout_file)
+    imputed, merfish = imputed_counts.X, true_counts_adata.X
+    return np.sqrt(np.square(np.subtract(imputed, merfish)).mean())
+
 
 warnings.filterwarnings("ignore")
 
@@ -33,20 +35,20 @@ if __name__ == '__main__':
     n_components_params = [1, 5, 10, 20, 40, 50, 100]
     alpha_W_params = [0, 0.2, 0.5, 0.7, 1]
     dropout_files = []
-    params = []
+    parameters = []
     for dims in n_components_params:
         for alpha_W in alpha_W_params:
             file = input_path + f"nmf_dims={dims}_alpha={alpha_W}_imputed.h5ad"
-            params.append((dims, alpha_W))
+            parameters.append((dims, alpha_W))
             dropout_files.append(file)
 
     # using partial function to pass in default params
     run_evaluation_pipeline = partial(
         rmse,
-        counts_adata=merfish,
+        true_counts_adata=merfish,
     )
 
-    print("Processing running NMF imputation...")
+    print("Evaluating NMF imputation...")
     CPUS_TO_USE = os.cpu_count() // 3
     with Pool(CPUS_TO_USE) as p:
         rmse_column = p.map(run_evaluation_pipeline, dropout_files)
@@ -55,13 +57,13 @@ if __name__ == '__main__':
     # Adding non-imputed dropout counts
     n_components_column = (np.NAN,) + n_components_column
     alpha_W_column = (np.NAN,) + alpha_W_column
-    rmse_column = (rmse(dropout_file, merfish), ) + rmse_column
+    rmse_column = [rmse(dropout_file, merfish)] + rmse_column
 
     results = pd.DataFrame(
         {
             "n_components": n_components_column,
             "alpha_w": alpha_W_column,
-            "RMSE": rmse_columns,
+            "RMSE": rmse_column,
         }
     )
 
